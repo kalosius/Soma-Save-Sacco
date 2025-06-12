f8c5438fd8e7c3e44f2410d634e277e7ae89bb6c
Aloisius Kasozi <124684740+kalosius@users.noreply.github.com>
2025-06-12 16:11:06 +0300

    Final Shres

--- clients_portal/admin.py
+++ clients_portal/admin.py
@@ -3,5 +3,12 @@ from . models import CustomUser, ShareTransaction
 
 # Register your models here.
 admin.site.register(CustomUser)
-admin.site.register(ShareTransaction)
+
+
+class ShareTransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'transaction_type', 'number_of_shares', 'amount', 'timestamp')  # 👈 add timestamp
+    list_filter = ('transaction_type', 'status', 'timestamp')  # optional: filter by time
+    search_fields = ('user__username', 'user__email')
+
+admin.site.register(ShareTransaction, ShareTransactionAdmin)
 
