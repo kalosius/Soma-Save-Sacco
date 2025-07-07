aa6a345f5e653af797a2766fb4565b0b48199cda
Aloisius Kasozi <124684740+kalosius@users.noreply.github.com>
2025-07-07 12:19:08 +0300

    Normal Path

--- clients_portal/urls.py
+++ clients_portal/urls.py
@@ -4,10 +4,10 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', views.startt, name='home'),
+    path('hometest/', views.startt, name='home'),
 
-    path('client_dashboard', views.client_dashboard, name='client_dashboard'),
-    path('login/', views.login_view, name='login'),
+    path('client_dashboard/', views.client_dashboard, name='client_dashboard'),
+    path('', views.login_view, name='login'),
     path('register/', views.register_view, name='register_view'),
     path('logout/', views.logout_view, name='logout'),
     path('userprofile/', views.userprofile, name='userprofile'),
