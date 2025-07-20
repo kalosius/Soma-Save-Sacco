3202877da61e9db0bcc515789b9da62a487ecc17
kalosius <kalosius43@gmail.com>
2025-07-20 20:11:01 +0300

    Signals Loaded

--- clients_portal/apps.py
+++ clients_portal/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class ClientsPortalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'clients_portal'
+
+    def ready(self):
+        # Ensure models.py (with signals) is loaded
+        import clients_portal.models
