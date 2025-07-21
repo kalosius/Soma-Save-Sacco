08aaf2bac8e3543b2d172b5527625fda3da5748e
kalosius <kalosius43@gmail.com>
2025-07-21 20:04:09 +0300

    Url Modification

--- soma_save_sacco/urls.py
+++ soma_save_sacco/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('somasavesaccosuperadmin/', admin.site.urls),
     path('client-admin/', include('adminapp.urls')),
     path('', include('clients_portal.urls')),
     path('pwa/', include('pwa.urls')),
